40d95ffe6b8fb4d47dc575e73b87e0fe6ac8df52
kazan <kazantsev.06.2004@gmail.com>
2025-09-01 21:19:55 +0500

    complete

---
+++ MyThirdTestAppSpringBoot/src/main/java/ru/kazantsev/MyThirdTestAppSpringBoot/exception/UnsupportedCodeException.java
@@ -0,0 +1,9 @@
+package ru.kazantsev.MyThirdTestAppSpringBoot.exception;
+
+public class UnsupportedCodeException extends Exception
+{
+    public UnsupportedCodeException(String message)
+    {
+        super(message);
+    }
+}
---
+++ MyThirdTestAppSpringBoot/src/main/java/ru/kazantsev/MyThirdTestAppSpringBoot/exception/ValidationFailedException.java
@@ -0,0 +1,9 @@
+package ru.kazantsev.MyThirdTestAppSpringBoot.exception;
+
+public class ValidationFailedException extends Exception
+{
+    public ValidationFailedException(String message)
+    {
+        super(message);
+    }
+}
