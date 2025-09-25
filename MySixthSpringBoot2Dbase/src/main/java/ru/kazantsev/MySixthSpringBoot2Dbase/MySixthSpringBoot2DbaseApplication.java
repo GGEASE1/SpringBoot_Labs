006343b6c87d95cf13de8376a9c6aaab3e043ec6
kazan <kazantsev.06.2004@gmail.com>
2025-09-25 19:54:13 +0500

    complete

---
+++ MySixthSpringBoot2Dbase/src/main/java/ru/kazantsev/MySixthSpringBoot2Dbase/MySixthSpringBoot2DbaseApplication.java
@@ -0,0 +1,13 @@
+package ru.kazantsev.MySixthSpringBoot2Dbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySixthSpringBoot2DbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySixthSpringBoot2DbaseApplication.class, args);
+	}
+
+}
---
+++ MySixthSpringBoot2Dbase/src/test/java/ru/kazantsev/MySixthSpringBoot2Dbase/MySixthSpringBoot2DbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kazantsev.MySixthSpringBoot2Dbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySixthSpringBoot2DbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
